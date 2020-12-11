099506ce4d21a3808ed3b55f26dd3eb2559f968e
Collin Murdock <murdoccr@miamioh.edu>
2020-12-10 21:56:21 -0500

    first working endpoint

--- src/GameTrackerDB.js
+++ src/GameTrackerDB.js
@@ -1,7 +1,7 @@
 
 const mysql = require('mysql')
 
-export class GameTrackerDB {
+class GameTrackerDB {
 
     conn;
 
@@ -10,11 +10,19 @@ export class GameTrackerDB {
         this.conn = mysql.createConnection({
             host: 'game-tracker.c9xtyyhdg4du.us-east-2.rds.amazonaws.com',
             user: 'admin',
-            password: 'sS0wnenRupcpi8tyM0D7'
+            password: 'sS0wnenRupcpi8tyM0D7',
+            database: 'gametracker'
         })
-
-        
     }
 
-    
+    async getTeamPlayers(teamName) {
+        return new Promise((resolve, reject) => {
+            let query = 'CALL sp_getTeamPlayers("'+teamName+'")'
+            this.conn.query(query, (err, result) => {
+                return err ? reject(err) : resolve(result[0])
+            })
+        }) 
+    }
 }
+
+module.exports = GameTrackerDB
--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 
 const express = require('express')
-const mysql = require('mysql')
+const GameTrackerDB = require('./GameTrackerDB')
 
 const app = express()
 const port = 80
+const db = new GameTrackerDB()
 
 app.get('/', (req, res) => {
     res.sendFile('html/index.html', { root: '.'})
+})
+
+app.get('/api/v1/getTeamPlayers/:teamName', async (req, res) => {
+
+    // extract and validate param
+    name = req.params.teamName.replace('+',' ')
+    console.log(name) 
+
+    // get data
+    res.json(await db.getTeamPlayers(name))
 })
 
 app.listen(port, () => {
